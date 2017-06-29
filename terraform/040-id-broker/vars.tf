99e3c4b6812d4e8da17a068a8754f6d5f423b3e8
Phillip Shipley <phillip.shipley@gmail.com>
2017-06-29 10:37:49 -0400

    refactored memory, cpu, and desired_count to be variable based

--- terraform/040-id-broker/vars.tf
+++ terraform/040-id-broker/vars.tf
@@ -1,96 +1,134 @@
+variable "memory" {
+  default = "96"
+}
+
+variable "cpu" {
+  default = "250"
+}
+
 variable "app_name" {
-  type = "string"
+  type    = "string"
   default = "id-broker"
 }
+
 variable "app_env" {
   type = "string"
 }
+
 variable "vpc_default_sg_id" {
   type = "string"
 }
+
 variable "private_subnet_ids" {
   type = "list"
 }
+
 variable "vpc_id" {
   type = "string"
 }
+
 variable "ssl_policy" {
   type = "string"
 }
+
 variable "wildcard_cert_arn" {
   type = "string"
 }
+
 variable "logentries_set_id" {
   type = "string"
 }
+
 variable "idp_name" {
   type = "string"
 }
+
 variable "docker_image" {
   type = "string"
 }
+
 variable "db_name" {
   type = "string"
 }
+
 variable "ldap_admin_password" {
   type = "string"
 }
+
 variable "ldap_admin_username" {
   type = "string"
 }
+
 variable "ldap_base_dn" {
   type = "string"
 }
+
 variable "ldap_domain_controllers" {
   type = "string"
 }
+
 variable "ldap_use_ssl" {
   type = "string"
 }
+
 variable "ldap_use_tls" {
   type = "string"
 }
+
 variable "mailer_usefiles" {
-  type = "string"
+  type    = "string"
   default = "false"
 }
+
 variable "mailer_host" {
   type = "string"
 }
+
 variable "mailer_username" {
   type = "string"
 }
+
 variable "mailer_password" {
   type = "string"
 }
+
 variable "notification_email" {
   type = "string"
 }
+
 variable "migrate_pw_from_ldap" {
   type = "string"
 }
+
 variable "mysql_host" {
   type = "string"
 }
+
 variable "mysql_user" {
   type = "string"
 }
+
 variable "mysql_pass" {
   type = "string"
 }
+
 variable "ecs_cluster_id" {
   type = "string"
 }
+
 variable "ecsServiceRole_arn" {
   type = "string"
 }
+
 variable "subdomain" {
   type = "string"
 }
+
 variable "cloudflare_domain" {
   type = "string"
 }
-variable "ecs_desired_count" {
-  type = "string"
+
+variable "desired_count" {
+  type    = "string"
   default = "1"
 }
