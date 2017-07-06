4b138e95d8a10c2cfac6ef0b41a49de4cb266fdc
Billy Clark <billy_clark@sil.org>
2017-07-06 14:29:57 -0400

    needed to provide ability to pass through Google Analytics config.

--- terraform/060-simplesamlphp/main.tf
+++ terraform/060-simplesamlphp/main.tf
@@ -78,6 +78,7 @@ data "template_file" "task_def" {
     secret_salt            = "${random_id.secretsalt.hex}"
     idp_name               = "${var.idp_name}"
     trusted_ip_addresses   = "${join(",", var.trusted_ip_addresses)}"
+    analytics_id           = "${var.analytics_id}"
   }
 }
 
