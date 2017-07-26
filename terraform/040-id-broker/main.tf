5588332b808f964637d93da22ba7789c40030606
Matt Henderson <matt_henderson@sil.org>
2017-07-26 09:21:02 -0400

    Add IdP name to ALB Target Group name.

--- terraform/040-id-broker/main.tf
+++ terraform/040-id-broker/main.tf
@@ -18,7 +18,7 @@ resource "aws_alb" "alb" {
  * Create target group for ALB
  */
 resource "aws_alb_target_group" "broker" {
-  name                 = "tg-${var.app_name}-${var.app_env}"
+  name                 = "tg-${var.idp_name}-${var.app_name}-${var.app_env}"
   port                 = "80"
   protocol             = "HTTP"
   vpc_id               = "${var.vpc_id}"
