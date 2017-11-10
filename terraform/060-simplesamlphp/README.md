af0a705ec29d96df0784a024e9a2ed87a3f5d663
Phillip Shipley <phillip.shipley@gmail.com>
2017-11-10 15:00:29 -0500

    update terraform-modules version for asg update

--- terraform/060-simplesamlphp/README.md
+++ terraform/060-simplesamlphp/README.md
@@ -53,7 +53,7 @@ This module is used to create an ECS service running simpleSAMLphp.
 
 ```hcl
 module "cf_ips" {
-  source = "github.com/silinternational/terraform-modules//cloudflare/ips?ref=1.1.3"
+  source = "github.com/silinternational/terraform-modules//cloudflare/ips?ref=2.0.0"
 }
 
 module "ssp" {
--- terraform/070-id-sync/main.tf
+++ terraform/070-id-sync/main.tf
@@ -91,7 +91,7 @@ data "template_file" "task_def" {
 }
 
 module "ecsservice" {
-  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-only?ref=1.1.3"
+  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-only?ref=2.0.0"
   cluster_id         = "${var.ecs_cluster_id}"
   service_name       = "${var.idp_name}-${var.app_name}"
   service_env        = "${var.app_env}"
