f7eccccb59808e6d69af58e8fc7f57d405bb7c0d
Schparky <3172830+Schparky@users.noreply.github.com>
2019-02-28 18:36:29 -0700

    update variable changes for pw-manager redesign

--- terraform/050-pw-manager/vars.tf
+++ terraform/050-pw-manager/vars.tf
@@ -1,25 +1,4 @@
-variable "memory" {
-  default = "100"
-}
-
-variable "cpu" {
-  default = "64"
-}
-
-variable "logentries_set_id" {
-  type = "string"
-}
-
-variable "app_name" {
-  type    = "string"
-  default = "pw-manager"
-}
-
-variable "app_env" {
-  type = "string"
-}
-
-variable "vpc_id" {
+variable "alb_dns_name" {
   type = "string"
 }
 
@@ -27,143 +6,40 @@ variable "alb_https_listener_arn" {
   type = "string"
 }
 
-variable "api_subdomain" {
-  type = "string"
-}
-
-variable "cloudflare_domain" {
-  type = "string"
-}
-
-variable "id_broker_assertValidBrokerIp" {
-  description = "Whether or not to assert IP address for ID Broker API is trusted"
-  default     = "true"
-}
-
-variable "id_broker_validIpRanges" {
-  description = "List of valid IP ranges to ID Broker API"
-  type        = "list"
-}
-
-variable "idp_name" {
-  type = "string"
-}
-
-variable "idp_display_name" {
-  type = "string"
-}
-
-variable "idp_username_hint" {
-  type    = "string"
-  default = "Username or email address"
-}
-
 variable "alerts_email" {
   type = "string"
 }
 
-variable "support_email" {
+variable "api_subdomain" {
   type = "string"
 }
 
-variable "from_email" {
+variable "app_env" {
   type = "string"
 }
 
-variable "from_name" {
-  type = "string"
+variable "app_name" {
+  type        = "string"
+  default     = "pw-manager"
+  description = "Used in ECS service names and logs, best to leave as default."
 }
 
-variable "logo_url" {
-  type = "string"
+variable "auth_saml_checkResponseSigning" {
+  default = "true"
 }
 
-variable "db_name" {
-  type = "string"
+variable "auth_saml_entityId" {
+  description = "SP entity ID"
+  type        = "string"
 }
 
-variable "mysql_host" {
-  type = "string"
+variable "auth_saml_idpCertificate" {
+  description = "Public cert data for IdP"
+  type        = "string"
 }
 
-variable "mysql_user" {
-  type = "string"
-}
-
-variable "mysql_pass" {
-  type = "string"
-}
-
-variable "recaptcha_key" {
-  type = "string"
-}
-
-variable "recaptcha_secret" {
-  type = "string"
-}
-
-variable "docker_image" {
-  type = "string"
-}
-
-variable "ui_subdomain" {
-  type = "string"
-}
-
-variable "id_broker_access_token" {
-  type = "string"
-}
-
-variable "id_broker_base_uri" {
-  type = "string"
-}
-
-variable "ecs_cluster_id" {
-  type = "string"
-}
-
-variable "ecsServiceRole_arn" {
-  type = "string"
-}
-
-variable "alb_dns_name" {
-  type = "string"
-}
-
-variable "wildcard_cert_arn" {
-  type = "string"
-}
-
-variable "cd_user_username" {
-  type = "string"
-}
-
-variable "desired_count" {
-  type    = "string"
-  default = "1"
-}
-
-variable "email_service_useEmailService" {
-  description = "Whether or not to use Email Service API to send emails"
-  default     = "true"
-}
-
-variable "email_service_baseUrl" {
-  description = "Base URL to Email Service API"
-}
-
-variable "email_service_accessToken" {
-  description = "Access Token for Email Service API"
-}
-
-variable "email_service_assertValidIp" {
-  description = "Whether or not to assert IP address for Email Service API is trusted"
-  default     = "true"
-}
-
-variable "email_service_validIpRanges" {
-  description = "List of valid IP ranges to Email Service API"
-  type        = "list"
+variable "auth_saml_requireEncryptedAssertion" {
+  default = "true"
 }
 
 variable "auth_saml_signRequest" {
@@ -172,16 +48,8 @@ variable "auth_saml_signRequest" {
   default     = "true"
 }
 
-variable "auth_saml_checkResponseSigning" {
-  default = "true"
-}
-
-variable "auth_saml_requireEncryptedAssertion" {
-  default = "true"
-}
-
-variable "auth_saml_idpCertificate" {
-  description = "Public cert data for IdP"
+variable "auth_saml_sloUrl" {
+  description = "SLO url for IdP"
   type        = "string"
 }
 
@@ -195,17 +63,183 @@ variable "auth_saml_spPrivateKey" {
   type        = "string"
 }
 
-variable "auth_saml_entityId" {
-  description = "SP entity ID"
-  type        = "string"
-}
-
 variable "auth_saml_ssoUrl" {
   description = "SSO url for IdP"
   type        = "string"
 }
 
-variable "auth_saml_sloUrl" {
-  description = "SLO url for IdP"
+variable "cd_user_username" {
+  type = "string"
+}
+
+variable "cloudflare_domain" {
+  type = "string"
+}
+
+variable "code_length" {
+  description = "Number of digits in reset code."
   type        = "string"
+  default     = "6"
+}
+
+variable "cpu" {
+  type        = "string"
+  description = "Amount of CPU to allocate to container, recommend '250' for production"
+  default     = "64"
+}
+
+variable "db_name" {
+  type = "string"
+}
+
+variable "desired_count" {
+  type    = "string"
+  default = "1"
+}
+
+variable "docker_image" {
+  type = "string"
+}
+
+variable "ecs_cluster_id" {
+  type = "string"
+}
+
+variable "ecsServiceRole_arn" {
+  type = "string"
+}
+
+variable "email_service_accessToken" {
+  description = "Access Token for Email Service API"
+}
+
+variable "email_service_assertValidIp" {
+  description = "Whether or not to assert IP address for Email Service API is trusted"
+  default     = "true"
+}
+
+variable "email_service_baseUrl" {
+  description = "Base URL to Email Service API"
+}
+
+variable "email_service_validIpRanges" {
+  description = "List of valid IP ranges to Email Service API"
+  type        = "list"
+}
+
+variable "from_name" {
+  description = "Name to send emails from"
+  type        = "string"
+}
+
+variable "id_broker_access_token" {
+  type = "string"
+}
+
+variable "id_broker_assertValidBrokerIp" {
+  description = "Whether or not to assert IP address for ID Broker API is trusted"
+  default     = "true"
+}
+
+variable "id_broker_base_uri" {
+  type = "string"
+}
+
+variable "id_broker_validIpRanges" {
+  description = "List of valid IP ranges to ID Broker API"
+  type        = "list"
+}
+
+variable "idp_display_name" {
+  description = "Display name of IdP for UI, something like 'ACME Inc.'"
+  type        = "string"
+}
+
+variable "idp_name" {
+  description = "Short name of IdP for logs, something like 'acme'"
+  type        = "string"
+}
+
+variable "idp_username_hint" {
+  type    = "string"
+  default = "Username or email address"
+}
+
+variable "logentries_set_id" {
+  type = "string"
+}
+
+variable "memory" {
+  description = "Amount of memory to allocate to container, recommend '128' for production"
+  default     = "100"
+}
+
+variable "mysql_host" {
+  type = "string"
+}
+
+variable "mysql_pass" {
+  type = "string"
+}
+
+variable "mysql_user" {
+  type = "string"
+}
+
+variable "password_rule_maxlength" {
+  description = "maximum password length"
+  type        = "string"
+  default     = "255"
+}
+
+variable "password_rule_minlength" {
+  description = "minimum password length"
+  type        = "string"
+  default     = "10"
+}
+
+variable "password_rule_minscore" {
+  description = "minimum password score"
+  type        = "string"
+  default     = "3"
+}
+
+variable "recaptcha_key" {
+  type = "string"
+}
+
+variable "recaptcha_secret" {
+  type = "string"
+}
+
+variable "support_email" {
+  description = "Email address for end user support, displayed on PW UI"
+  type        = "string"
+}
+
+variable "support_feedback" {
+  description = "Email address for end user feedback, displayed on PW UI"
+  type        = "string"
+}
+
+variable "support_phone" {
+  description = "Phone number for end user support, displayed on PW UI"
+  type        = "string"
+}
+
+variable "support_url" {
+  description = "URL for end user support, displayed on PW UI"
+  type        = "string"
+}
+
+variable "ui_subdomain" {
+  type = "string"
+}
+
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "wildcard_cert_arn" {
+  type = "string"
 }
