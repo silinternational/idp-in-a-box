bf5c56dccce42caa33f2d4a1185b5f08a79ec785
Schparky <3172830+Schparky@users.noreply.github.com>
2020-04-06 10:49:07 -0600

    31 days is not a valid retention value; change to 30

--- terraform/010-cluster/main.tf
+++ terraform/010-cluster/main.tf
@@ -93,7 +93,7 @@ resource "logentries_logset" "logset" {
  */
 resource "aws_cloudwatch_log_group" "logs" {
   name              = "idp-${var.idp_name}-${var.app_env}"
-  retention_in_days = 31
+  retention_in_days = 30
 
   tags {
     idp_name = "${var.idp_name}"
