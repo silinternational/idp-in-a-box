78bde7a51bff6c4251e403135d67b5f4deb01dd5
Schparky <3172830+Schparky@users.noreply.github.com>
2020-04-08 14:22:37 -0600

    add "cron" to task definition family

--- terraform/040-id-broker/main.tf
+++ terraform/040-id-broker/main.tf
@@ -282,7 +282,7 @@ data "template_file" "task_def_cron" {
  * Create cron task definition
  */
 resource "aws_ecs_task_definition" "cron_td" {
-  family                = "${var.idp_name}-${var.app_name}-${var.app_env}"
+  family                = "${var.idp_name}-${var.app_name}-cron-${var.app_env}"
   container_definitions = "${data.template_file.task_def_cron.rendered}"
   network_mode          = "bridge"
 }
