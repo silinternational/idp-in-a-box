c46de70e483ee94169f1f71b36dc6c5612792e8c
Schparky <3172830+Schparky@users.noreply.github.com>
2020-04-27 08:47:27 -0600

    add idp_name to db-backup IAM role to avoid naming conflict

--- terraform/032-db-backup/main.tf
+++ terraform/032-db-backup/main.tf
@@ -98,7 +98,7 @@ data "template_file" "task_def_backup" {
  * Create role for scheduled running of cron task definitions.
  */
 resource "aws_iam_role" "ecs_events" {
-  name = "ecs_events-${var.app_name}-${var.app_env}"
+  name = "ecs_events-${var.idp_name}-${var.app_name}-${var.app_env}"
 
   assume_role_policy = <<EOF
 {
