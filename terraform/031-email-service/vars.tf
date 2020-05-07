93b24617f878f5d011a9e996381b151fd2d9f73b
Schparky <3172830+Schparky@users.noreply.github.com>
2020-05-07 13:27:10 -0600

    Revert "add SES task role to 031-email-service"

--- terraform/031-email-service/vars.tf
+++ terraform/031-email-service/vars.tf
@@ -68,6 +68,10 @@ variable "from_email" {
   type = "string"
 }
 
+variable "from_name" {
+  type = "string"
+}
+
 variable "idp_name" {
   type = "string"
 }
@@ -82,9 +86,21 @@ variable "internal_alb_listener_arn" {
   type        = "string"
 }
 
+variable "mailer_host" {
+  type = "string"
+}
+
+variable "mailer_password" {
+  type = "string"
+}
+
 variable "mailer_usefiles" {
   type    = "string"
   default = "false"
+}
+
+variable "mailer_username" {
+  type = "string"
 }
 
 variable "memory_api" {
