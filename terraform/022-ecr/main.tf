9ecadbbd5b00cd517db2907a5e3566b9704e19cb
Schparky <3172830+Schparky@users.noreply.github.com>
2023-05-04 14:15:53 +0800

    add ECR lifecycle policies

--- terraform/022-ecr/main.tf
+++ terraform/022-ecr/main.tf
@@ -2,65 +2,77 @@
  * id-broker
  */
 module "ecr_idbroker" {
-  source              = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
-  repo_name           = "${var.idp_name}/id-broker"
-  ecsInstanceRole_arn = var.ecsInstanceRole_arn
-  ecsServiceRole_arn  = var.ecsServiceRole_arn
-  cd_user_arn         = var.cd_user_arn
+  source                = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
+  repo_name             = "${var.idp_name}/id-broker"
+  ecsInstanceRole_arn   = var.ecsInstanceRole_arn
+  ecsServiceRole_arn    = var.ecsServiceRole_arn
+  cd_user_arn           = var.cd_user_arn
+  image_retention_count = 10
+  image_retention_tags  = ["latest"]
 }
 
 /*
  * email-service
  */
 module "ecr_emailservice" {
-  source              = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
-  repo_name           = "${var.idp_name}/email-service"
-  ecsInstanceRole_arn = var.ecsInstanceRole_arn
-  ecsServiceRole_arn  = var.ecsServiceRole_arn
-  cd_user_arn         = var.cd_user_arn
+  source                = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
+  repo_name             = "${var.idp_name}/email-service"
+  ecsInstanceRole_arn   = var.ecsInstanceRole_arn
+  ecsServiceRole_arn    = var.ecsServiceRole_arn
+  cd_user_arn           = var.cd_user_arn
+  image_retention_count = 10
+  image_retention_tags  = ["latest"]
 }
 
 /*
  * pw-api
  */
 module "ecr_pwapi" {
-  source              = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
-  repo_name           = "${var.idp_name}/pw-api"
-  ecsInstanceRole_arn = var.ecsInstanceRole_arn
-  ecsServiceRole_arn  = var.ecsServiceRole_arn
-  cd_user_arn         = var.cd_user_arn
+  source                = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
+  repo_name             = "${var.idp_name}/pw-api"
+  ecsInstanceRole_arn   = var.ecsInstanceRole_arn
+  ecsServiceRole_arn    = var.ecsServiceRole_arn
+  cd_user_arn           = var.cd_user_arn
+  image_retention_count = 10
+  image_retention_tags  = ["latest"]
 }
 
 /*
  * simplesamlphp
  */
 module "ecr_simplesamlphp" {
-  source              = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
-  repo_name           = "${var.idp_name}/simplesamlphp"
-  ecsInstanceRole_arn = var.ecsInstanceRole_arn
-  ecsServiceRole_arn  = var.ecsServiceRole_arn
-  cd_user_arn         = var.cd_user_arn
+  source                = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
+  repo_name             = "${var.idp_name}/simplesamlphp"
+  ecsInstanceRole_arn   = var.ecsInstanceRole_arn
+  ecsServiceRole_arn    = var.ecsServiceRole_arn
+  cd_user_arn           = var.cd_user_arn
+  image_retention_count = 10
+  image_retention_tags  = ["latest"]
 }
 
 /*
  * id-sync
  */
 module "ecr_idsync" {
-  source              = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
-  repo_name           = "${var.idp_name}/id-sync"
-  ecsInstanceRole_arn = var.ecsInstanceRole_arn
-  ecsServiceRole_arn  = var.ecsServiceRole_arn
-  cd_user_arn         = var.cd_user_arn
+  source                = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
+  repo_name             = "${var.idp_name}/id-sync"
+  ecsInstanceRole_arn   = var.ecsInstanceRole_arn
+  ecsServiceRole_arn    = var.ecsServiceRole_arn
+  cd_user_arn           = var.cd_user_arn
+  image_retention_count = 10
+  image_retention_tags  = ["latest"]
 }
 
 /*
  * db-backup
  */
 module "ecr_dbbackup" {
-  source              = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
-  repo_name           = "${var.idp_name}/db-backup"
-  ecsInstanceRole_arn = var.ecsInstanceRole_arn
-  ecsServiceRole_arn  = var.ecsServiceRole_arn
-  cd_user_arn         = var.cd_user_arn
+  source                = "github.com/silinternational/terraform-modules//aws/ecr?ref=8.0.1"
+  repo_name             = "${var.idp_name}/db-backup"
+  ecsInstanceRole_arn   = var.ecsInstanceRole_arn
+  ecsServiceRole_arn    = var.ecsServiceRole_arn
+  cd_user_arn           = var.cd_user_arn
+  image_retention_count = 10
+  image_retention_tags  = ["latest"]
 }
 
