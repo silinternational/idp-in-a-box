4a9d79939edcf7caaf75941b1e0bca580106cc6b
briskt <3172830+briskt@users.noreply.github.com>
2023-05-18 18:11:54 +0800

    update the test

--- test/000-core.tf
+++ test/000-core.tf
@@ -1,27 +1,24 @@
 module "defaults" {
   source = "../terraform/000-core"
 
-  app_name    = "test"
-  app_env     = "testing"
-  cert_domain = "example.com"
+  cert_domain  = "example.com"
+  cluster_name = "test-cluster-1"
 }
 
 module "create_acm_cert" {
   source = "../terraform/000-core"
 
-  app_name        = "test"
-  app_env         = "testing"
   cert_domain     = "example.com"
   create_acm_cert = true
+  cluster_name    = "test-cluster-2"
 }
 
 module "no_create_user" {
   source = "../terraform/000-core"
 
-  app_name       = "test"
-  app_env        = "testing"
   cert_domain    = "example.com"
   create_cd_user = false
+  cluster_name   = "test-cluster-3"
 }
 
 provider "aws" {
