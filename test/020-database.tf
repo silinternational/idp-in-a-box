c96ebb22cd86f2fcb2993aaa2031207e6bee6bd0
briskt <3172830+briskt@users.noreply.github.com>
2023-05-22 12:07:26 +0800

    add test case for a read replica

--- test/020-database.tf
+++ test/020-database.tf
@@ -8,3 +8,17 @@ module "defaults" {
   security_groups   = ["sg-12345"]
   subnet_group_name = "db-subnet-test"
 }
+
+module "defaults" {
+  source = "../terraform/020-database"
+
+  app_env           = "test"
+  app_name          = "test"
+  mysql_user        = "root"
+  db_name           = "db"
+  security_groups   = ["sg-12345"]
+  subnet_group_name = "db-subnet-test"
+
+  create_passwords    = false
+  replicate_source_db = "arn:aws:rds:us-east-1:123456789012:db:dummy"
+}
