8d59fb135f5692bdf0d286a068ff8831a6fe2eb1
briskt <3172830+briskt@users.noreply.github.com>
2023-06-12 18:31:15 +0800

    first start at creating a CLI tool

---
+++ cmd/cli/root.go
@@ -0,0 +1,94 @@
+/*
+Copyright © 2023 SIL International
+*/
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var defaultsFile string
+
+func Execute() {
+	rootCmd := &cobra.Command{
+		Use:   "idp",
+		Short: "idp-in-a-box CLI",
+		Long: `idp is a CLI tool for the silinternational/idp-in-a-box system.
+It can be used to check the status of the IdP. It can also be used to establish secondary resources
+in a second AWS region, and to initiate a secondary region failover action.`,
+	}
+
+	rootCmd.PersistentFlags().StringVar(&defaultsFile, "defaults", "", "Defaults file")
+
+	var org string
+	rootCmd.PersistentFlags().StringVar(&org, "org", "", "Terraform Cloud organization")
+	if err := viper.BindPFlag("org", rootCmd.PersistentFlags().Lookup("org")); err != nil {
+		log.Fatalln("unable to bind flag: ", err)
+	}
+
+	var token string
+	rootCmd.PersistentFlags().StringVar(&token, "token", "", "Terraform Cloud token")
+	if err := viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token")); err != nil {
+		log.Fatalln("unable to bind flag: ", err)
+	}
+
+	var idp string
+	rootCmd.PersistentFlags().StringVar(&idp, "idp", "", "IDP key (short name)")
+	if err := viper.BindPFlag("idp", rootCmd.PersistentFlags().Lookup("idp")); err != nil {
+		log.Fatalln("unable to bind flag: ", err)
+	}
+
+	setupStatusCmd(rootCmd)
+
+	cobra.OnInitialize(initDefaults)
+
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+// initDefaults reads in a Defaults file and ENV variables if set.
+func initDefaults() {
+	if defaultsFile != "" {
+		// Use defaults file from the flag.
+		viper.SetConfigFile(defaultsFile)
+	} else {
+		// Find home directory and add ~/.config/ to the config search path
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+		viper.AddConfigPath(home + "/.config")
+
+		// Add the current directory to the search path
+		viper.AddConfigPath(".")
+
+		// Set the config file name to "idp", allowing any supported file extension, e.g. idp.toml
+		viper.SetConfigName("idp")
+	}
+
+	// look for environment variables that match the uppercase of the viper key, prefixed with "IDP_"
+	viper.SetEnvPrefix("IDP")
+	viper.AutomaticEnv()
+
+	// If a defaults file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		_, err = fmt.Fprintln(os.Stderr, "Using defaults file:", viper.ConfigFileUsed())
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		var vErr viper.ConfigFileNotFoundError
+		if !errors.As(err, &vErr) {
+			_, err = fmt.Fprintf(os.Stderr, "Problem with defaults file: %s %T\n", err, err)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+	}
+}
---
+++ cmd/cli/status.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2023 SIL International
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/silinternational/tfc-ops/lib"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Read the current status of the IdP",
+	Long:  `Read the current status of the IdP. Does not modify any infrastructure.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		status()
+	},
+}
+
+func setupStatusCmd(rootCmd *cobra.Command) {
+	rootCmd.AddCommand(statusCmd)
+}
+
+func status() {
+	idp := viper.GetString("idp")
+	if idp == "" {
+		log.Fatalln("no IdP key is set, use --idp to set the IdP key")
+	}
+
+	org := viper.GetString("org")
+	if org == "" {
+		log.Fatalln("no org is set, use --org to set the Terraform Cloud org")
+	}
+
+	token := viper.GetString("token")
+	if token == "" {
+		log.Fatalln("no token is set, use --token to set the Terraform Cloud API token")
+	}
+	lib.SetToken(token)
+
+	workspaceName := fmt.Sprintf("idp-%s-prod-000-core", idp)
+	failoverActive, err := lib.GetWorkspaceVar(org, workspaceName, "aws_failover_active")
+	if err != nil {
+		return
+	}
+
+	if failoverActive.Value == "true" {
+		fmt.Println("IdP Failover is ACTIVE")
+	} else {
+		fmt.Println("IdP Failover is NOT active")
+	}
+}
