4d63e20e3f782cfe8fe2665d170dfb9f88f8c708
briskt <3172830+briskt@users.noreply.github.com>
2023-06-13 19:04:09 +0800

    multiregion setup - set variables in cloned workspaces

--- cmd/cli/multiregion/setup.go
+++ cmd/cli/multiregion/setup.go
@@ -79,41 +79,101 @@ func cloneWorkspace(pFlags PersistentFlags, workspace string) {
 func setMultiregionVariables(pFlags PersistentFlags) {
 	fmt.Println("\nsetting variables...")
 
+	// Set variables in primary workspaces that also point to secondary workspaces
+
 	coreVars := []lib.TFVar{
 		{Key: "aws_create_secondary", Value: "true"},
 		{Key: "aws_failover_active", Value: "false"},
 		{Key: "aws_region_secondary", Value: pFlags.secondaryRegion},
 	}
-	if err := setVars(pFlags, coreWorkspace(pFlags), coreVars); err != nil {
-		return
-	}
+	setVars(pFlags, coreWorkspace(pFlags), coreVars)
 
 	backupVars := []lib.TFVar{
 		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
 		{Key: "tf_remote_database_secondary", Value: pFlags.org + "/" + databaseSecondaryWorkspace(pFlags)},
 	}
-	if err := setVars(pFlags, backupWorkspace(pFlags), backupVars); err != nil {
-		return
+	setVars(pFlags, backupWorkspace(pFlags), backupVars)
+
+	// Set variables in the new secondary workspaces
+
+	clusterVars := []lib.TFVar{
+		{Key: "aws_zones", Value: getZonesHCL(pFlags.secondaryRegion), Hcl: true},
 	}
+	setVars(pFlags, clusterSecondaryWorkspace(pFlags), clusterVars)
+
+	databaseVars := []lib.TFVar{
+		{Key: "availability_zone", Value: pFlags.secondaryRegion + "a"}, // TODO: make this work in all regions
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_database", Value: pFlags.org + "/" + databaseWorkspace(pFlags)},
+	}
+	setVars(pFlags, databaseSecondaryWorkspace(pFlags), databaseVars)
+
+	pmaVars := []lib.TFVar{
+		{Key: "pma_subdomain", Value: pFlags.idp + "-pma-secondary"},
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_database_secondary", Value: pFlags.org + "/" + databaseSecondaryWorkspace(pFlags)},
+	}
+	setVars(pFlags, pmaSecondaryWorkspace(pFlags), pmaVars)
+
+	emailVars := []lib.TFVar{
+		{Key: "email_subdomain", Value: pFlags.idp + "-email-secondary"},
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_database_secondary", Value: pFlags.org + "/" + databaseSecondaryWorkspace(pFlags)},
+	}
+	setVars(pFlags, emailSecondaryWorkspace(pFlags), emailVars)
+
+	brokerVars := []lib.TFVar{
+		{Key: "broker_subdomain", Value: pFlags.idp + "-broker-secondary"},
+		{Key: "mfa_totp_apibaseurl", Value: "TODO"},     // TODO: get this value
+		{Key: "mfa_webauthn_apibaseurl", Value: "TODO"}, // TODO: get this value
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_database_secondary", Value: pFlags.org + "/" + databaseSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_email_secondary", Value: pFlags.org + "/" + emailSecondaryWorkspace(pFlags)},
+	}
+	setVars(pFlags, brokerSecondaryWorkspace(pFlags), brokerVars)
+
+	pwVars := []lib.TFVar{
+		{Key: "api_subdomain", Value: pFlags.idp + "-pw-api-secondary"},
+		{Key: "ui_subdomain", Value: pFlags.idp + "-pw-secondary"},
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_database_secondary", Value: pFlags.org + "/" + databaseSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_email_secondary", Value: pFlags.org + "/" + emailSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_broker_secondary", Value: pFlags.org + "/" + brokerSecondaryWorkspace(pFlags)},
+	}
+	setVars(pFlags, pwSecondaryWorkspace(pFlags), pwVars)
+
+	sspVars := []lib.TFVar{
+		{Key: "ssp_subdomain", Value: pFlags.idp + "-secondary"},
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_database_secondary", Value: pFlags.org + "/" + databaseSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_broker_secondary", Value: pFlags.org + "/" + brokerSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_pwmanager_secondary", Value: pFlags.org + "/" + pwSecondaryWorkspace(pFlags)},
+	}
+	setVars(pFlags, sspSecondaryWorkspace(pFlags), sspVars)
+
+	syncVars := []lib.TFVar{
+		{Key: "sync_subdomain", Value: pFlags.idp + "-sync-secondary"},
+		{Key: "tf_remote_cluster_secondary", Value: pFlags.org + "/" + clusterSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_email_secondary", Value: pFlags.org + "/" + emailSecondaryWorkspace(pFlags)},
+		{Key: "tf_remote_broker_secondary", Value: pFlags.org + "/" + brokerSecondaryWorkspace(pFlags)},
+	}
+	setVars(pFlags, syncSecondaryWorkspace(pFlags), syncVars)
 }
 
 // setVars sets a list of variables using the current variable values to decide whether to update or create
-func setVars(pFlags PersistentFlags, workspace string, newVars []lib.TFVar) error {
+func setVars(pFlags PersistentFlags, workspace string, newVars []lib.TFVar) {
 	currentVars, err := lib.GetVarsFromWorkspace(pFlags.org, workspace)
 	if err != nil {
 		log.Fatalf("failed to get the variables from %q", workspace)
 	}
 
 	for _, v := range newVars {
-		if err := setVar(pFlags, currentVars, workspace, v); err != nil {
-			log.Fatalf("Error: could not set variable %q in workspace %q: %s", v.Key, workspace)
-		}
+		setVar(pFlags, currentVars, workspace, v)
 	}
-	return nil
 }
 
 // setVar sets a variable using the list of current variables to decide whether to update or create
-func setVar(pFlags PersistentFlags, vars []lib.Var, workspace string, tfVar lib.TFVar) error {
+func setVar(pFlags PersistentFlags, vars []lib.Var, workspace string, tfVar lib.TFVar) {
 	if v := findVar(vars, tfVar.Key); v == nil {
 		fmt.Printf("%s - creating var.%s with value %q\n", workspace, tfVar.Key, tfVar.Value)
 		if !pFlags.readOnlyMode {
@@ -122,14 +182,13 @@ func setVar(pFlags PersistentFlags, vars []lib.Var, workspace string, tfVar lib.
 	} else {
 		if v.Value == tfVar.Value {
 			fmt.Printf("%s - var.%s is already set to %q\n", workspace, tfVar.Key, tfVar.Value)
-			return nil
+			return
 		}
 		fmt.Printf("%s - setting var.%s to %q\n", workspace, tfVar.Key, tfVar.Value)
 		if !pFlags.readOnlyMode {
 			lib.UpdateVariable(pFlags.org, workspace, v.ID, tfVar)
 		}
 	}
-	return nil
 }
 
 // findVar locates a variable by its key
@@ -140,4 +199,16 @@ func findVar(vars []lib.Var, key string) *lib.Var {
 		}
 	}
 	return nil
+}
+
+func getZonesHCL(region string) string {
+	// TODO: call the AWS EC2 API to get the actual list of zones. This list is safe for us-east-1, us-east-2, and us-west-2, at least.
+
+	const template = `[
+  "%[1]sa", 
+  "%[1]sb", 
+  "%[1]sc",
+  "%[1]sd",
+]`
+	return fmt.Sprintf(template, region)
 }
