6cc18b895e1c782cf3ec61b0b54e3eb4b80185f4
Matt H <matt_henderson@sil.org>
2023-07-11 13:46:37 -0500

    Fix formatting

--- terraform/032-db-backup/main.tf
+++ terraform/032-db-backup/main.tf
@@ -13,8 +13,8 @@ resource "aws_s3_bucket" "backup" {
 }
 
 resource "aws_s3_bucket_acl" "backup" {
-  bucket = aws_s3_bucket.backup.id
-  acl    = "private"
+  bucket     = aws_s3_bucket.backup.id
+  acl        = "private"
   depends_on = [aws_s3_bucket_ownership_controls.backup]
 }
 
