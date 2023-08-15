341c23a9ad48c3d01dd26d3f5e00f796112c96f9
Matt H <matt_henderson@sil.org>
2023-08-15 12:04:20 -0400

    Update all uses of terraform-modules to version 8.6.0

--- terraform/031-email-service/main.tf
+++ terraform/031-email-service/main.tf
@@ -85,7 +85,7 @@ locals {
 }
 
 module "ecsservice_api" {
-  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-only?ref=8.5.0"
+  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-only?ref=8.6.0"
   cluster_id         = var.ecs_cluster_id
   service_name       = "${var.idp_name}-${var.app_name}-api"
   service_env        = var.app_env
@@ -126,7 +126,7 @@ locals {
 }
 
 module "ecsservice_cron" {
-  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-no-alb?ref=8.5.0"
+  source             = "github.com/silinternational/terraform-modules//aws/ecs/service-no-alb?ref=8.6.0"
   cluster_id         = var.ecs_cluster_id
   service_name       = "${var.idp_name}-${var.app_name}-cron"
   service_env        = var.app_env
