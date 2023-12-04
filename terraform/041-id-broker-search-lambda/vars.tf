3f2204db15c283b210525e15063f3aa8976a1fe3
briskt <3172830+briskt@users.noreply.github.com>
2023-12-04 16:13:38 -0700

    Change lambda runtime default to provided.al2

--- terraform/041-id-broker-search-lambda/vars.tf
+++ terraform/041-id-broker-search-lambda/vars.tf
@@ -23,7 +23,7 @@ variable "function_zip_name" {
 }
 
 variable "function_name" {
-  default = "idp-id-broker-search"
+  default = "bootstrap"
 }
 
 variable "idp_name" {
@@ -31,8 +31,8 @@ variable "idp_name" {
 }
 
 variable "lambda_runtime" {
-  description = "AWS Lambda runtime environment, either `provided.al2` or `go1.x`. `go1.x` is deprecated"
-  default     = "go1.x"
+  description = "AWS Lambda runtime environment"
+  default     = "provided.al2"
   type        = string
 }
 
