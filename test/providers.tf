53f10e67aa4185ef39f3892e512fb17a4a65c4a7
briskt <3172830+briskt@users.noreply.github.com>
2024-11-21 20:40:31 +0800

    ignore terraform_required_providers in the test folder

--- test/providers.tf
+++ test/providers.tf
@@ -1,9 +1,11 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
+    # tflint-ignore: terraform_required_providers
     aws = {
       source = "hashicorp/aws"
     }
+    # tflint-ignore: terraform_required_providers
     cloudflare = {
       source = "cloudflare/cloudflare"
     }
