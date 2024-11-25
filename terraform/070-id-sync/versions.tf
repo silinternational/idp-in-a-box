b567ed4ee32d62e189701d965eafd7d773f2ff37
briskt <3172830+briskt@users.noreply.github.com>
2024-11-25 11:11:42 +0800

    remove unused Cloudflare provider in 070-id-sync

--- terraform/070-id-sync/versions.tf
+++ terraform/070-id-sync/versions.tf
@@ -6,13 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0.0, < 6.0.0"
     }
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-
-      // 4.39.0 deprecated cloudflare_record.value
-      // While waiting for version 5 to mature, we'll constrain to earlier versions.
-      version = ">= 2.0.0, < 4.39.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = ">= 2.2.0, < 4.0.0"
