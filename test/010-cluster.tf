0367ecdf68d9b2b82ddfa9515d40422814381152
Praveen <praveen_koilraj@sil.org>
2024-12-09 11:56:43 +0530

    test fix

--- test/010-cluster.tf
+++ test/010-cluster.tf
@@ -1,15 +1,16 @@
 module "cluster" {
   source = "../terraform/010-cluster"
 
-  app_name                 = ""
-  app_env                  = ""
-  aws_instance             = { a = "b" }
-  aws_zones                = [""]
-  cert_domain_name         = ""
-  create_nat_gateway       = true
-  ecs_cluster_name         = ""
-  ecs_instance_profile_id  = ""
-  idp_name                 = ""
-  asg_additional_user_data = ""
-  tags                     = {}
+  app_name                       = ""
+  app_env                        = ""
+  aws_instance                   = { a = "b" }
+  aws_zones                      = [""]
+  cert_domain_name               = ""
+  create_nat_gateway             = true
+  ecs_cluster_name               = ""
+  ecs_instance_profile_id        = ""
+  idp_name                       = ""
+  asg_additional_user_data       = ""
+  tags                           = {}
+  enable_ec2_detailed_monitoring = ""
 }
